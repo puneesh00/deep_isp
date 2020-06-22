a24bcd185e1e368a920986db98de69d9267ca20f
Bhavya Vasudeva <30321205+estija@users.noreply.github.com>
2020-06-22 19:02:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,98 @@
+import network as network
+import tensorflow as tf
+import numpy as np
+import pickle
+from keras.optimizers import Adam
+import argparse
+from keras.utils import multi_gpu_model 
+from keras.applications.vgg16 import VGG16
+import os
+from keras.models import Model
+from keras import backend as K
+from load_data import load_training_batch
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-e' ,'--epochs', type = int, default = 100, help = 'number of epochs for training')
+parser.add_argument('-b' ,'--batch_size', type = int, default = 32, help = 'batch size for training')
+parser.add_argument('-l' ,'--log_file', type = str, default = log, help = 'log file name to be saved')
+parser.add_argument('-exp' ,'--experiment_title', type = str, default = isp_learning, help = 'experiment title is used as a folder name to save respective files')
+parser.add_argument('-w' ,'--weights_file', type = str, default = weights , help = 'weight file name to be appended while saving')
+parser.add_argument('-o' ,'--optimizer_weights', type = str, default = opt, help = 'optimizer file name to be appended while saving')
+parser.add_argument('-lr' ,'--learning_rate', type = float, default = 0.0001, help = 'initial learning rate for the optimizer')
+
+args = parser.parse_args()
+n_epochs = args.epochs
+n_batch = args.batch_size
+log_file = args.log_file
+weights_file = args.weights_file
+lr = args.learning_rate
+opt_file = args.optimizer_weights
+exp_folder = args.experiment_title
+
+current_path = os.getcwd()
+os.mkdir(os.path.join(current_path, exp_folder))
+
+def mssim(y_true, y_pred):
+  costs = 1.0 - tf.reduce_mean(tf.image.ssim(y_true, y_pred, 2.0))
+  return costs
+
+def color(y_true, y_pred):
+  ytn = K.l2_normalize(y_true, axis=-1)
+  ypn = K.l2_normalize(y_pred, axis=-1)
+  color_cos = tf.einsum('...i,...i->...', ytn, ypn)
+  color_angle = tf.math.acos(color_cos)
+  ca_mean = tf.reduce_mean(color_angle)
+  return ca_mean
+
+def vgg_loss(y_true, y_pred):
+  vgg_mse = tf.keras.losses.MSE(y_true, y_pred)
+  return vgg_mse
+
+def train(d_par, d_model, n_epochs, n_batch, f, current_path, exp_folder, weights_file):
+    
+    train_size = 5000
+    bat_per_epo = int(train_size/n_batch)
+    
+    for i in range(n_epochs):
+
+        raw, canon = load_training_batch(dataset_dir, train_size, PATCH_WIDTH=224, PATCH_HEIGHT=224, DSLR_SCALE=1)
+
+        for j in range(bat_per_epo):
+            ix = np.random.randint(0, train_size, n_batch)
+            
+            X_real = canon[ix]
+            X_in  = raw[ix]
+            
+            d_loss = d_par.train_on_batch(X_in,[X_real,X_real,X_real,vgg.predict(X_real)])
+            
+            f.write('>%d, %d/%d, d=%.3f, mae=%.3f,  mssim=%.3f, color=%.3f, vgg=%.3f' %(i+1, j+1, bat_per_epo, d_loss[0], d_loss[1], d_loss[2], d_loss[3], d_loss[4]))
+            f.write('\n')
+            print('>%d, %d/%d, d=%.3f, mae=%.3f,  mssim=%.3f, color=%.3f, vgg=%.3f' %(i+1, j+1, bat_per_epo, d_loss[0], d_loss[1], d_loss[2], d_loss[3], d_loss[4]))
+        filename = os.path.join(current_path, exp_folder, weights_file + '_%04d.h5' % (i+1))
+        d_save = d_par.get_layer('model_3')
+        d_save.save_weights(filename)
+        del raw
+        del canon
+        # if (i+1) % 10 == 0:
+        # summarize_performance (i, g_model, d_model, dataset)
+    f.close()
+
+
+in_shape = (224,224,4)
+
+base_vgg=VGG16(weights='imagenet',include_top=False,input_shape=(224,224,3))
+vgg=Model(inputs=base_vgg.input,outputs=base_vgg.get_layer('block4_pool').output)
+vgg.summary()
+
+d_model = network(inp_shape = in_shape, trainable = True, vgg)
+d_model.summary()
+d_par = multi_gpu_model(d_model, gpus=4, cpu_relocation = True)
+opt = Adam(lr = lr, beta_1 = 0.5)
+d_par.compile(loss = ['mae',mssim,color,vgg_loss], optimizer = opt, loss_weights = [0.01, 20.0, 1.0, 10.0])
+
+f = open(os.path.join(current_path, exp_folder, log_file + '.txt'), 'x')
+f = open(os.path.join(current_path, exp_folder, log_file + '.txt'), 'a')
+
+train(d_par, d_model, n_epochs, n_batch, f, current_path, exp_folder, weights_file)
---
+++ modules (2).py
@@ -0,0 +1,117 @@
+from keras.layers import Conv2D, Dense, GlobalAveragePooling2D, Reshape, Concatenate, Multiply
+from keras.layers import PReLU, BatchNormalization, Add
+
+
+def squeeze_excite(inp_layer, ratio):
+
+    '''
+    inp_layer : input tensor
+    ratio : speaks for itself
+    '''
+
+    filters = inp_layer._keras_shape[-1]
+
+
+    se = GlobalAveragePooling2D()(inp_layer)
+    se = Reshape((1, 1, filters))(se)
+    se = Dense(filters // ratio, activation = 'relu', kernel_initializer = 'he_normal', use_bias = True, bias_initializer = 'zeros')(se)
+    se = Dense(filters, activation = 'sigmoid', kernel_initializer = 'he_normal', use_bias = True, bias_initializer = 'zeros')(se)
+
+    se = Multiply()[inp_layer, se]
+
+    return se
+
+
+
+def inception(inp, f1, f13, f3, f133, f33, f5, gamma_init, trainable):
+
+    '''
+    f1 : no. of feature maps(channels) from receptive field 1x1
+
+    f13: no. of features maps(channels) from 1x1 input to 3x3
+    f3: no. of feature maps(channels) out from receptive field 3x3
+
+    f133: no. of features maps(channels) from 1x1 input to next two 3x3s
+    f33: no. of feature maps(channels) from 3x3 to 3x3
+    f5: no. of feature maps(channels) out from receptive field 5x5
+
+    inp_layer: input tensor
+    '''
+
+    x1 = Conv2D(f1, (1,1), strides = (1,1), padding = 'same', use_bias = True, kernel_initializer = 'he_normal', bias_initializer = 'zeros')(inp)
+    x1 = BatchNormalization(gamma_initializer = gamma_init, trainable = trainable)(x1)
+    x1 = PReLU(shared_axes=[1,2])(x1)
+
+    x3 = Conv2D(f13, (1,1), strides = (1,1), padding = 'same', use_bias = True, kernel_initializer = 'he_normal', bias_initializer = 'zeros')(inp)
+    x3 = BatchNormalization(gamma_initializer = gamma_init, trainable = trainable)(x3)
+    x3 = PReLU(shared_axes=[1,2])(x3)
+    x3 = Conv2D(f3, (3,3), strides = (1,1), padding = 'same', use_bias = True, kernel_initializer = 'he_normal', bias_initializer = 'zeros')(x3)
+    x3 = BatchNormalization(gamma_initializer = gamma_init, trainable = trainable)(x3)
+    x3 = PReLU(shared_axes=[1,2])(x3)
+
+    x5 = Conv2D(f133, (1,1), strides = (1,1), padding = 'same', use_bias = True, kernel_initializer = 'he_normal', bias_initializer = 'zeros')(inp)
+    x5 = BatchNormalization(gamma_initializer = gamma_init, trainable = trainable)(x5)
+    x5 = PReLU(shared_axes=[1,2])(x5)
+    x5 = Conv2D(f33, (3,3), strides = (1,1), padding = 'same', use_bias = True, kernel_initializer = 'he_normal', bias_initializer = 'zeros')(x5)
+    x5 = BatchNormalization(gamma_initializer = gamma_init, trainable = trainable)(x5)
+    x5 = PReLU(shared_axes=[1,2])(x5)
+    x5 = Conv2D(f5, (3,3), strides = (1,1), padding = 'same', use_bias = True, kernel_initializer = 'he_normal', bias_initializer = 'zeros')(x5)
+    x5 = BatchNormalization(gamma_initializer = gamma_init, trainable = trainable)(x5)
+    x5 = PReLU(shared_axes=[1,2])(x5)
+
+    x5 = Concatenate(axis = -1)([x1, x3, x5])
+
+    return x5
+
+
+def rise(x, f1, f13, f3, f133, f33, f5, ratio, gamma_init, trainable):
+  x1 = inception(x, f1, f13, f3, f133, f33, f5, gamma_init, trainable)
+  x1 = squeeze_excite(x1, ratio)
+  x1 = Add()([x1, x])
+  return x1
+
+
+def dil_out(x, d, m, gamma_init, trainable):
+
+  '''
+  d : no. of feature maps
+  m : dilation rate 
+  x: input tensor
+  '''
+
+  x=ZeroPadding2D(padding=2**m)(x)
+  x=Conv2D(filters = d, kernel_size = 3, strides = 1, dilation_rate = m, padding = 'valid', use_bias = True, kernel_initializer = 'he_normal', bias_initializer = 'zeros')(x)
+  x=BatchNormalization(gamma_initializer = gamma_init, trainable = trainable)(x)
+  x=PReLU(shared_axes=[1,2])(x)
+  return x
+
+def espy(x, d, level, gamma_init, trainable):
+    '''
+    d : no. of feature maps
+    level : no. of levels of dilation rates (1,...,level)
+    x: input tensor
+    '''
+
+    x0=Conv2D(filters=d, kernel_size=1, strides=1, padding='same', use_bias = True, kernel_initializer = 'he_normal', bias_initializer = 'zeros')(x)
+    x0=BatchNormalization(gamma_initializer = gamma_init, trainable = trainable)(x0)
+    x0=PReLU(shared_axes=[1,2])(x0)
+
+    x1 = dil_out(x0,d,1,gamma_init,trainable)
+    x1c = x1
+
+    for m in range(level-1):
+      x2 = dil_out(x0,d,m+2,gamma_init,trainable)
+      x2 = Add()([x1,x2])
+      x1 = x2
+      x2 = Concatenate(axis=-1)([x1c,x2])
+      x1c = x2
+
+    x_out = Add()([x,x2])
+
+    return x_out
+
+def conv(x,ch,k,s,gamma_init,trainable):
+  x = Conv2D(ch, k, strides = s, padding = 'same', use_bias = True, kernel_initializer = 'he_normal', bias_initializer = 'zeros')(x)
+  x = BatchNormalization(gamma_initializer = gamma_init, trainable = trainable)(x)
+  x = PReLU(shared_axes=[1,2])(x)
+  return x
