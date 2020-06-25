53720f22bcfbd35d6a3ebda6d07b2c263b18427a
estija <bhavyavasudeva10@gmail.com>
2020-06-26 00:18:46 +0530

    minor changes

--- infer_full.py
+++ infer_full.py
@@ -3,6 +3,7 @@ from network import network
 import tensorflow as tf
 import numpy as np
 import pickle
+import imageio
 from metrics import metrics
 import argparse
 from keras.utils import multi_gpu_model
@@ -28,9 +29,9 @@ dataset_dir = args.dataset_path
 
 current_path = '/home/sp-lab-2/deep_isp_exps'
 
-in_shape = (1984,1488,4)
+in_shape = (1488,1984,4)
 
-base_vgg = VGG16(weights = 'imagenet', include_top = False, input_shape = (3968,2976,3))
+base_vgg = VGG16(weights = 'imagenet', include_top = False, input_shape = (2976,3968,3))
 vgg = Model(inputs = base_vgg.input, outputs = base_vgg.get_layer('block4_pool').output)
 for layer in vgg.layers:
      layer.trainable = False
@@ -43,13 +44,13 @@ img_dir = dataset_dir + '/test/huawei_full_resolution/'
 img_nos = [1167,1614,2169,252,2978,3163,3379,4489,5776,6645]
 
 for img in img_nos:
-	I = np.asarray(imageio.imread((train_directory_phone + str(img) + '.png')))
+	I = np.asarray(imageio.imread((img_dir + str(img) + '.png')))
 	I = extract_bayer_channels(I)
 	print(I.shape)
-	I = np.reshape(I, [1, 1984, 1488, 4])
+	I = np.reshape(I, [1, 1488, 1984, 4])
 	out,_,_,_ = d_model.predict(I)
-	out = np.reshape(out, [3968, 2976, 3])
-    misc.imsave(current_path + exp_folder + str(img) + "_res.png", out)
+	out = np.reshape(out, [2976, 3968, 3])
+	misc.imsave(current_path + exp_folder + str(img) + "_res.png", out)
 
 
 
