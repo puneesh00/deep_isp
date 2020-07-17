6f0e65833502dd99409c32b03f567c84503af054
Puneesh Deora <puneesh6298@gmail.com>
2020-07-18 00:27:28 +0530

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,5 +1,13 @@
 from keras.layers import Conv2D, Conv2DTranspose, DepthwiseConv2D, Dense, GlobalAveragePooling2D, Reshape, Concatenate, Multiply
-from keras.layers import PReLU, BatchNormalization, Add, Lambda
+from keras.layers import Layer, LeakyReLU, PReLU, BatchNormalization, Add, Lambda
+import tensorflow as tf
+
+def SubpixelConv2D(scale = 2):
+    def subpixel(x):
+        import tensorflow as tf
+        return tf.depth_to_space(x, scale)
+    return Lambda(subpixel)    
+
 
 def crop(start, end):
     # Crops (or slices) a Tensor on a given dimension from start to end
@@ -9,8 +17,25 @@ def crop(start, end):
        return x[:, :, :, start: end]
     return Lambda(func)
 
-def squeeze_excite(inp_layer, ratio):
 
+class adapwt(Layer):
+    def __init__(self, **kwargs):
+        #self.output_dim = output_dim
+        super(adapwt, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        # Create a trainable weight variable for this layer.
+        self.W = self.add_weight(name='wt',shape=[1,1],initializer='uniform',trainable=True)
+        super(adapwt, self).build(input_shape)  # Be sure to call this at the end
+
+    def call(self, x):
+        return x*self.W
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
+def squeeze_excite(inp_layer, ratio):
     '''
     inp_layer : input tensor
     ratio : speaks for itself
@@ -27,7 +52,6 @@ def squeeze_excite(inp_layer, ratio):
     se = Multiply()([inp_layer, se])
 
     return se
-
 
 
 def inception(inp, f1, f13, f3, f133, f33, f5, gamma_init, trainable):
@@ -71,9 +95,11 @@ def inception(inp, f1, f13, f3, f133, f33, f5, gamma_init, trainable):
     return x5
 
 
-def rise(x, f1, f13, f3, f133, f33, f5, ratio, gamma_init, trainable):
+def rise(x, f1, f13, f3, f133, f33, f5, ratio, gamma_init, trainable, beta_tr=False):
   x1 = inception(x, f1, f13, f3, f133, f33, f5, gamma_init, trainable)
   x1 = squeeze_excite(x1, ratio)
+  if beta_tr:
+    x=adapwt()(x)
   x1 = Add()([x1, x])
   return x1
 
@@ -92,7 +118,8 @@ def dil_out(x, d, m, gamma_init, trainable):
   x=PReLU(shared_axes=[1,2])(x)
   return x
 
-def espy(x, d, level, gamma_init, trainable):
+
+def espy(x, d, level, gamma_init, trainable, beta_tr=False):
     '''
     d : no. of feature maps
     level : no. of levels of dilation rates (1,...,level)
@@ -108,11 +135,15 @@ def espy(x, d, level, gamma_init, trainable):
 
     for m in range(level-1):
       x2 = dil_out(x0, d, m+2, gamma_init, trainable)
+      if beta_tr:
+        x1=adapwt()(x1)
       x2 = Add()([x1, x2])
       x1 = x2
       x2 = Concatenate(axis = -1)([x1c, x2])
       x1c = x2
 
+    if beta_tr:
+      x=adapwt()(x)
     x_out = Add()([x, x2])
 
     return x_out
@@ -127,6 +158,18 @@ def conv_trans(x, ch, k, s, gamma_init, trainable):
   x = Conv2DTranspose(ch, k, strides = s, padding = 'same', use_bias = True, kernel_initializer = 'he_normal', bias_initializer = 'zeros')(x)
   x = BatchNormalization(gamma_initializer = gamma_init, trainable = trainable)(x)
   x = PReLU(shared_axes = [1,2])(x)
+  return x
+
+def convl(x, ch, k, s, gamma_init, trainable):
+  x = Conv2D(ch, k, strides = s, padding = 'same', use_bias = True, kernel_initializer = 'he_normal', bias_initializer = 'zeros')(x)
+  #x = BatchNormalization(gamma_initializer = gamma_init, trainable = trainable)(x)
+  x = LeakyReLU(alpha = 0.2)(x)
+  return x
+
+def convl_trans(x, ch, k, s, gamma_init, trainable):
+  x = Conv2DTranspose(ch, k, strides = s, padding = 'same', use_bias = True, kernel_initializer = 'he_normal', bias_initializer = 'zeros')(x)
+  #x = BatchNormalization(gamma_initializer = gamma_init, trainable = trainable)(x)
+  x = LeakyReLU(alpha = 0.2)(x)
   return x
 
 def depthwise_conv(x, ch, k, s, gamma_init, trainable):
